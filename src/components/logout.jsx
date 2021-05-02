107721045d8ab492436140f85f2a3f7430b04218
Mohammad Eskini <mreskini30@gmail.com>
2021-05-02 22:24:44 +0300

    cleaned the logout component code

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -1,8 +1,14 @@
+//requirements
 import { useHistory } from "react-router-dom";
+
 export default function Logout(){
+
+    //properties
     const history = useHistory()
+
     localStorage.removeItem("user_token")
     history.push("/login")
+
     return(
         <></>
     );
